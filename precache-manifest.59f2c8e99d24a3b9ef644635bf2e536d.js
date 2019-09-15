a34361140f2cd60114d1c3acb3900eabb67e9256
Klement Tan <klementtan@gmail.com>
2019-09-15 16:43:18 +0800

    Updates

---
+++ precache-manifest.59f2c8e99d24a3b9ef644635bf2e536d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285ca06e8f8ecee6a6adda2724891244",
+    "url": "docs.klementtan.com/index.html"
+  },
+  {
+    "revision": "b94c6c48ddc4dae8da72",
+    "url": "docs.klementtan.com/static/css/2.2202bcb7.chunk.css"
+  },
+  {
+    "revision": "37c321d1b164727bfe1d",
+    "url": "docs.klementtan.com/static/css/main.9394c38a.chunk.css"
+  },
+  {
+    "revision": "b94c6c48ddc4dae8da72",
+    "url": "docs.klementtan.com/static/js/2.8d2ec389.chunk.js"
+  },
+  {
+    "revision": "37c321d1b164727bfe1d",
+    "url": "docs.klementtan.com/static/js/main.e3edff11.chunk.js"
+  },
+  {
+    "revision": "a7c97f40e8e482c0a7e7",
+    "url": "docs.klementtan.com/static/js/runtime~main.485fa2c0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "docs.klementtan.com/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "docs.klementtan.com/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "docs.klementtan.com/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "docs.klementtan.com/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "docs.klementtan.com/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "docs.klementtan.com/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "docs.klementtan.com/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "docs.klementtan.com/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "docs.klementtan.com/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "docs.klementtan.com/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "docs.klementtan.com/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "docs.klementtan.com/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "docs.klementtan.com/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "docs.klementtan.com/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "docs.klementtan.com/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "docs.klementtan.com/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
